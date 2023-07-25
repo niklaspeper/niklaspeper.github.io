bb610c3a6ab3ff01439ea740fdbf02ab4962a103
niklas <niklasp96@gmail.com>
2023-07-25 13:55:49 +0200

    added config

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -5,6 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/scss/variables.scss";'
+      }
+    }
+  },
   plugins: [
     vue(),
   ],
